50a540af0c5b7ac8dd7bd61fb77ac7928302e65e
gbrwg <c3g2c@ugrad.cs.ubc.ca>
2023-02-11 23:57:26 -0800

    work on addDataset

--- src/controller/Helper.ts
+++ src/controller/Helper.ts
@@ -17,22 +17,24 @@ interface Dataset{
 }
 
 export default class Helper {
+	private datasets: any;
 	public async processData(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		let zip: JSZip = await JSZip.loadAsync(content, {base64: true});
 		let data: any[] = [];
 		// https://stackoverflow.com/questions/39939644/jszip-checking-if-a-zip-folder-contains-a-specific-file
 		if (zip.folder(/courses/).length > 0) {
-			data = await this.getZip(zip);
+			data = await this.getZipFilesContent(zip);
 		} else {
 			throw new InsightError();
 		}
 
-		return Promise.resolve([]);
+		let json = await this.getJSON(data);
+
+		return Promise.resolve(json);
 
 	}
 
-
-	public async getZip(zip: JSZip): Promise<Section[]>{
+	private getZipFilesContent(zip: JSZip): Promise<string[]>{
 		let files: any[] = [];
 		zip.forEach((relativePath, file) => {
 			files.push(file.async("text"));
@@ -41,5 +43,34 @@ export default class Helper {
 		return Promise.resolve(files);
 	}
 
+	private getJSON(data: string[]): Promise<any[]> {
+		let sections: any[] = [];
+		return Promise.all(
+			data.map((file) => {
+				const res = JSON.parse(file).result;
+				const fileSections = [];
+				for (const r of res) {
+					fileSections.push({
+						uuid: String(r.id),
+						id: r.course,
+						title: r.Title,
+						instructor: r.Professor,
+						dept: r.Subject,
+						avg: r.Avg,
+						pass: r.Pass,
+						fail: r.Fail,
+						audi: r.Audit,
+						year: r.Section === "overall" ? 1900 : Number(r.year),
+					});
+				}
+				return fileSections;
+			})
+		).then((results) => {
+			for (const result of results) {
+				sections = sections.concat(result);
+			}
+			return sections;
+		});
+	}
 
 }
--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -18,8 +18,10 @@ import Helper from "./Helper";
  */
 export default class InsightFacade implements IInsightFacade {
 	private helper: Helper;
+	private datasets: Map<string, any>;
 	constructor() {
 		this.helper = new Helper();
+		this.datasets = new Map<string, any[]>();
 		console.log("InsightFacadeImpl::init()");
 	}
 
@@ -27,24 +29,37 @@ export default class InsightFacade implements IInsightFacade {
 		return new Promise<string[]>((resolve, reject) => {
 			// !id.trim() from https://stackoverflow.com/questions/10261986/how-to-detect-string-which-contains-only-spaces
 			if (kind !== "sections" || id === "" || id.includes("_") || !id.trim()) {
-				reject(new InsightError());
+				reject(new InsightError("invalid ID"));
 			}
-
 			// check duplicate
-
+			if (this.datasets.has(id)) {
+				reject(new InsightError("Cannot add existing ID"));
+			}
 			// parse file
-			this.helper.processData(id, content, kind)
-				.then((result) =>
-					resolve(result))
-				.catch((error) =>
-					reject(error));
 
-			reject("Not implemented.");
+			this.helper.processData(id, content, kind)
+				.then((result) => {
+					this.datasets.set(id, result);
+					resolve(this.datasets.get(id));
+				})
+				.catch(() => {
+					reject(new InsightError());
+				});
 		});
 	}
 
 	public removeDataset(id: string): Promise<string> {
-		return Promise.reject("Not implemented.");
+
+		return new Promise<string>((resolve, reject) => {
+			if (id === "" || id.includes("_") || !id.trim()) {
+				reject(new InsightError("invalid ID"));
+			} else if (!this.datasets.has(id)) {
+				reject(new NotFoundError("Cannot delete non-existent ID"));
+			} else {
+				this.datasets.delete(id);
+				resolve(id);
+			}
+		});
 	}
 
 	public performQuery(query: unknown): Promise<InsightResult[]> {
